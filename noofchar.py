c1f339a13225ee28cc4b49cb71e9423473a6c6f0
achintya-kumar-rai <achintyakumarrai0025@gmail.com>
2024-07-25 06:46:01 -0700

    code to tell idex and no of charater present in the string

---
+++ noofchar.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+no_of_char=0
+index=0
+for i in name :
+    print("the character",i,"is indexed at",index )
+    no_of_char+=1
+    index+=1
+print("number of character in your name is ", no_of_char)
